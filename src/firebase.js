e2320c54862d3db2098369e2b6a8c2c251892712
muhreynaldii <reynaldi180101@gmail.com>
2022-05-20 17:31:22 +0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "tutorial-50310.firebaseapp.com",
+  projectId: "tutorial-50310",
+  storageBucket: "tutorial-50310.appspot.com",
+  messagingSenderId: "726154916485",
+  appId: "1:726154916485:web:13d3b967f59b216372efec",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
